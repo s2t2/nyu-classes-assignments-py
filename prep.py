e8d5072c1f6b69ffcef6706ef1f76a5dc0c6d56a
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-04 14:28:38 -0400

    Locating attachment files for each student

--- prep.py
+++ prep.py
@@ -4,19 +4,21 @@ import os
 from data import students
 
 SUBMISSIONS_DIRPATH = os.path.join(os.path.dirname(__file__), "submissions")
-
-student_dirs = [d for d in os.listdir(SUBMISSIONS_DIRPATH) if d not in [".DS_Store", ".gitignore", "grades.csv"]]
-student_dirs.sort()
+dirs = os.listdir(SUBMISSIONS_DIRPATH)
+dirs = [d for d in dirs if d not in [".DS_Store", ".gitignore", "grades.csv"]]
+dirs.sort()
 
 print("---------------------")
-print(f"PROCESSING {len(student_dirs)} DIRECTORIES:")
-
-groups = []
-
-for dirname in student_dirs:
+print(f"PROCESSING {len(dirs)} DIRECTORIES:")
+for dirname in dirs:
     parts = dirname.split("(")
     student_name = parts[0]
-    net_id = parts[-1].replace(")","")
+    net_id = parts[-1].replace(")", "")
     group_id = [s["group_id"] for s in students if s["net_id"] == net_id][0]
     print("...", group_id, net_id)
+
     #breakpoint()
+    attachments_dirpath = os.path.join(os.path.dirname(__file__), "submissions", dirname, "Submission attachment(s)")
+    attachment_filenames = os.listdir(attachments_dirpath)
+    for attachment_filename in attachment_filenames:
+        print("...", "...", attachment_filename)
