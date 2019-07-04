82cf84fb07c6e5ca93f1d1825f99ccd48f1e3c83
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-04 14:43:42 -0400

    Copy and rename files

--- prep.py
+++ prep.py
@@ -1,24 +1,30 @@
 
 import os
+import shutil
 
 from data import students
 
-SUBMISSIONS_DIRPATH = os.path.join(os.path.dirname(__file__), "submissions")
-dirs = os.listdir(SUBMISSIONS_DIRPATH)
+STUDENTS_DIRPATH = os.path.join(os.path.dirname(__file__), "submissions")
+GROUPS_DIRPATH = os.path.join(os.path.dirname(__file__), "group_submissions")
+
+dirs = os.listdir(STUDENTS_DIRPATH)
 dirs = [d for d in dirs if d not in [".DS_Store", ".gitignore", "grades.csv"]]
 dirs.sort()
 
 print("---------------------")
 print(f"PROCESSING {len(dirs)} DIRECTORIES:")
+
 for dirname in dirs:
     parts = dirname.split("(")
-    student_name = parts[0]
+    #student_name = parts[0]
     net_id = parts[-1].replace(")", "")
     group_id = [s["group_id"] for s in students if s["net_id"] == net_id][0]
-    print("...", group_id, net_id)
 
-    #breakpoint()
-    attachments_dirpath = os.path.join(os.path.dirname(__file__), "submissions", dirname, "Submission attachment(s)")
+    attachments_dirpath = os.path.join(STUDENTS_DIRPATH, dirname, "Submission attachment(s)")
     attachment_filenames = os.listdir(attachments_dirpath)
     for attachment_filename in attachment_filenames:
-        print("...", "...", attachment_filename)
+        attachment_filepath = os.path.join(attachments_dirpath, attachment_filename)
+        print("...", group_id, net_id, attachment_filename)
+
+        group_submission_filepath = os.path.join(GROUPS_DIRPATH, f"{group_id}.pdf")
+        shutil.copyfile(attachment_filepath, group_submission_filepath)
